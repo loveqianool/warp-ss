2930664bd506a4eb742cf37c8468017f04b97397
zhuYe <loveqianool@users.noreply.github.com>
2022-05-14 19:35:18 +0800

    Update z.sh

--- z.sh
+++ z.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -ex
 
+if [ ! -f "/etc/shadowsocks-rust/config.json" ]; then
+ echo ss 配置文件不存在
+else
+ echo 启动ss
+ssserver -c /etc/shadowsocks-rust/config.json &
+fi
+
+if [ ! -f "/etc/v2ray/config.json" ]; then
+ echo v2 配置文件不存在
+else
+ echo 启动v2
+v2ray run -c /etc/v2ray/config.json &
+fi
+
 wg-quick up wg0
 
 sleep infinity & wait
