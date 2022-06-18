79ec790e214585c7de6f0df395957caa4e331de1
zhuYe <loveqianool@users.noreply.github.com>
2022-06-18 18:08:34 +0800

    Update z.sh

--- z.sh
+++ z.sh
@@ -1,31 +1,31 @@
 #!/bin/sh -ex
 
 if [ ! -f "/etc/wireguard/wg0.conf" ]; then
- # warp 配置文件不存在
+ echo warp 配置文件不存在
 else
- # 启动warp
+ echo 启动warp
 wg-quick up wg0
 sleep 3
 fi
 
 if [ ! -f "/etc/shadowsocks-rust/config.json" ]; then
- # ss 配置文件不存在
+ echo ss 配置文件不存在
 else
- # 启动ss
+ echo 启动ss
 ssserver -c /etc/shadowsocks-rust/config.json &
 fi
 
 if [ ! -f "/etc/v2ray/config.json" ]; then
- # v2 配置文件不存在
+ echo v2 配置文件不存在
 else
- # 启动v2
+ echo 启动v2
 v2ray run -c /etc/v2ray/config.json &
 fi
 
 if [ ! -f "/etc/xray/config.json" ]; then
- # xray 配置文件不存在
+ echo xray 配置文件不存在
 else
- # 启动xray
+ echo 启动xray
 xray run -c /etc/xray/config.json &
 fi
 
