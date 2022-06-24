604be0d1f93f26e29b693aeb27a80998fe497caf
zhuYe <loveqianool@users.noreply.github.com>
2022-06-24 23:49:38 +0800

    Create z.sh

---
+++ debian/z.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -ex
+
+if [ ! -f "/etc/wireguard/wg0.conf" ]; then
+ echo warp 配置文件不存在
+else
+ echo 启动 warp
+wg-quick up wg0
+sleep 3
+fi
+
+if [ ! -f "/etc/shadowsocks-rust/config.json" ]; then
+ echo ss 配置文件不存在
+else
+ echo 启动 ss
+ssserver -c /etc/shadowsocks-rust/config.json &
+fi
+
+if [ ! -f "/etc/v2ray/config.json" ]; then
+ echo v2 配置文件不存在
+else
+ echo 启动v2
+v2ray run -c /etc/v2ray/config.json &
+fi
+
+if [ ! -f "/etc/xray/config.json" ]; then
+ echo xray 配置文件不存在
+else
+ echo 启动 xray
+xray run -c /etc/xray/config.json &
+fi
+
+sleep infinity & wait
